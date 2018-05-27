76aef8c94189f3480c368282f05cf81c640d71cf
etokec <etoke.c@gmail.com>
2018-05-27 15:37:48 -0700

    Lesson 11-12
    11- Events, Refs and this Binding
    12 - Handling Events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,14 +2,29 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
-	render(){
+	myInput = React.createRef();
+	
+	goToStore =(event) => {
+		//1. Stop the form from submitting
+		event.preventDefault();
+		//2.get the text from that input
+		const storeName=this.myInput.value.value;
+		//3.Change the page to /store/wtv-they-submitted
+		this.props.history.push(`/store/${storeName}`);
+	}
+	render() {
 		return (
 			<React.Fragment>
 				{/*This a comment, It must be INSIDE
 				 the return element*/}
-				<form className="store-selector"> 
+				<form className="store-selector" onSubmit={this.goToStore}> 
 					<h2>Please Enter a Store</h2>
-					<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+					<input 
+						type="text" 
+						ref={this.myInput}
+						required placeholder="Store Name"
+						defaultValue={getFunName()}
+					/>
 					<button type="submit">Visit Store ➡</button>
 				</form>
 			</React.Fragment>
